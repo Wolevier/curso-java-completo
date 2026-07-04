646f14e7de857f2403e2c594fb8adb8cf7a0d47b
Wagner Almeida de Oliveira <131931327+Wolevier@users.noreply.github.com>
2026-07-04 18:30:25 -0300

    style: removendo comentários

--- src/secao06_estrutura_repeticao/Exercicio01Login.java
+++ src/secao06_estrutura_repeticao/Exercicio01Login.java
@@ -7,21 +7,18 @@ public class Exercicio01Login {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // Declaração limpa, sem precisar forçar valor inicial de string vazia
         String login;
         String password;
 
         System.out.println("=== SISTEMA DE ACESSO ===");
 
-        // O do-while roda primeiro e testa a condição depois
         do {
             System.out.print("Login: ");
-            login = sc.nextLine().trim(); // .trim() é padrão profissional para evitar espaços bobos
+            login = sc.nextLine().trim(); 
 
             System.out.print("Senha: ");
             password = sc.nextLine().trim();
 
-            // Validação simples e direta
             if (!login.equals("wolive") || !password.equals("1234")) {
                 System.out.println("Erro: Dados incorretos. Tente novamente.\n");
             }
